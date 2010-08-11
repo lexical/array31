1688775a91d556921a6532442aa40e14b7bcee17
Keng-Yu Lin <kengyu@lexical.tw>
2010-08-11 23:12:24 +0800

    Release under GPLv3+

--- arrayts40.cpp
+++ arrayts40.cpp
@@ -1,4 +1,23 @@
-﻿#include "aarrayts.h"
+﻿/*
+ *   Copyright 2010 Keng-Yu Lin <kengyu@lexical.tw>
+ *
+ *   This file is part of Array31.
+ *
+ *   Array31 is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Array31 is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Array31.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "aarrayts.h"
 
 HRESULT AArrayTextService::OnKeyDown40( ITfContext * pContext, WPARAM wParam, LPARAM lParam, BOOL * pfEaten )
 {
